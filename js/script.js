6009d407ffa419e35e2969a512694e2773f8de1e
Maxim <vrijheid.rromg@gmail.com>
2023-12-02 17:00:56 +0300

    mobile navbar fix

--- js/script.js
+++ js/script.js
@@ -2,13 +2,17 @@
 
 let menuButton = document.querySelector(".menu-button");
 let navMenu = document.querySelector(".aside");
+let main = document.querySelector(".wrapper");
 
 menuButton.addEventListener("click", function(){
   navMenu.classList.toggle("aside_mobile");
+  main.classList.toggle("hide-overflow");
 })
 
 navMenu.addEventListener("click", function(event){
   let link = event.target.closest(".aside__link")
   if (!link) return;
-  setTimeout(()=>navMenu.classList.toggle("aside_mobile"), 50);
+  main.classList.toggle("hide-overflow");
+  navMenu.classList.toggle("aside_mobile");
+
 })
